e495e2add79022e1760ff61f8f18e47e15d9f5e5
461 <461.00.kd@gmail.com>
2016-04-14 15:59:29 +0300

    deleted classes dao and plug

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,8 @@
  * Created by 461 on 14.04.2016.
  */
 
-import plug.PlugDataSource;
-
 public class Main {
     public static void main(String[] args) {
-        PlugDataSource dataSource = new PlugDataSource();
+
     }
 }
